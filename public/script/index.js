efe0442781e1a22d7a07949b7cd84a93c2b4094e
fcy <Feng.chaoying@outlook.com>
2017-11-02 15:14:00 +0800

    表格

--- public/script/index.js
+++
@@ -1,7 +0,0 @@
-// $(function () {
-//     $('.tabOption').click(function () {
-//         $(this).addClass('color').siblings().removeClass('color')
-//         console.log($(this).text())
-//     })
-
-// });
