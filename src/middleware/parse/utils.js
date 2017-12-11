22be887cb00636cfecd746aa6ed5be9c4c5833ea
zkeyword <zjx1247225@163.com>
2017-12-11 23:17:44 +0800

    处理字符串解析错误

--- src/middleware/parse/utils.js
+++ src/middleware/parse/utils.js
@@ -1,9 +1,11 @@
 
 const drafter = require('drafter')
-const normalizeNewline = require('normalize-newline')
 const { mock } = require('mockjs')
+const normalizeNewline = require('normalize-newline')
 const commentJson = require('comment-json')
+const stripJsonComments = require('strip-json-comments')
 const stringifyObject = require('stringify-object')
+const SJSON = require('sjson')
 const urlParser = require('./url')
 
 function handleRtr(str, isRevert = false) {
@@ -79,10 +81,16 @@ exports.validateDrafterResult = result => {
 
 exports.jsonParse = (str, original) => {
     if (!str) return str
-    str = str.replace(/\'/g, '"')
+    // str = str.replace(/\'/g, '"')
     str = handleRtr(str, true)
     if (original) {
-        str = str.replace(/(\/\/.*)|(\/\*.*\*\/)/g, '')
+        str = stripJsonComments(str) // 清除注释
+        /* 无引号、单双引号的问题 */
+        str = str.replace(/(\w):\/\//g, '$1☜//') // 网址被误伤
+        str = SJSON.squish(str)
+        str = SJSON.unsquish(str)
+        str = str.replace(/(\w)☜\/\//g, '$1://')
+        console.log(SJSON.squish(str))
     } else {
         try {
             str = commentJson.parse(str)
